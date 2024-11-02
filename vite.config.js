db9d6e38b42983a2d0d98f9e69379fba346d6107
YasserDalali <yasser.dalali.personal@gmail.com>
2024-11-02 10:42:55 +0100

    deployment edits

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/japanese-Budoux/', // Change 'your-repo' to your actual GitHub repo name
+  build: {
+    outDir: 'dist', // This is the folder where build files will go
+  },
+})
