276e27ec80a0d5cca92d9e58c1a050e310175eb9
YasserDalali <yasser.dalali.personal@gmail.com>
2024-11-05 17:41:54 +0100

    refactor: stopped autoload of Budoux

--- src/budoux.js
+++ src/budoux.js
@@ -1,4 +1,5 @@
 import { loadDefaultJapaneseParser } from "budoux";
+import { loadDefaultThaiParser } from "budoux";
 
 function runParser() {
   if (document.documentElement.lang === "ja") {
@@ -6,12 +7,16 @@ function runParser() {
     const ele = document.querySelector("body");
     parser.applyToElement(ele);
   }
+  else if (document.documentElement.lang === "th") {
+    const parser = loadDefaultThaiParser();
+    const ele = document.querySelector("body");
+    parser.applyToElement(ele);
+  }
+
+  // runParser should be run:
+  // 1. On page load (After siteTran finishes transation and changes HTML lang)
+  // 2. Whenever the language is changed.
 }
-// Expose the runParser function so that it can be called
-// from the browser console if needed.
+
 window.runParser = runParser;
 // Run the parser as soon as the page's DOM has finished loading.
-window.onload = function () {
-  console.log("onload");
-  runParser();
-};
