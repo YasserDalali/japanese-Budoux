95789b7b25a9ae0fa2c3ad434e98d43ee700f096
YasserDalali <yasser.dalali.personal@gmail.com>
2024-11-05 10:59:07 +0100

    chore: JapaneseParser will apply only if page is in japanese

--- src/budoux.js
+++ src/budoux.js
@@ -1,52 +1,17 @@
-import {loadDefaultJapaneseParser} from "budoux";
+import { loadDefaultJapaneseParser } from "budoux";
 
-
-/**
- * Applies the Japanese parser to the element with class "word-wrap".
- * First, it stores the original text of the element in the data attribute "originalText".
- * Then, it runs the parser on the element.
- */
 function runParser() {
-    console.log("SUCCESS");
-    
-  const parser = loadDefaultJapaneseParser();
-  
-  const ele = document.querySelector("body");
-
-  parser.applyToElement(ele);
+  if (document.documentElement.lang === "ja") {
+    const parser = loadDefaultJapaneseParser();
+    const ele = document.querySelector("body");
+    parser.applyToElement(ele);
+  }
 }
-
-
-const fontsize = document.getElementById('fontsize');
-fontsize.addEventListener('input', (event) => {
-    const main = document.querySelector('body');
-    main.style.fontSize = `${event.target.value}rem`;
-})
-
-const buttonCheck = document.getElementById('WWcheckbox');
-buttonCheck.addEventListener('click', () => {
-        try { runParser(); }
-
-        catch (e) { console.error("error: " + e); }
-
-         finally { buttonCheck.classList.add('disabled'); } 
-    });
-
-
-
-    // Expose the runParser function so that it can be called
-    // from the browser console if needed.
-    window.runParser = runParser;
-    // Run the parser as soon as the page's DOM has finished loading.
-    window.onload = function() {
-        console.log("onload");
-        runParser();
-    };
-
-/* const selectCSS = document.getElementById('selectCSS'); 
-const p = document.querySelector('p');
-selectCSS.addEventListener('change', () => {
-    const lineBreakVal = selectCSS.value;
-    p.className = `break-${lineBreakVal}`;
-    console.log(document.body.style)
-}); */
+// Expose the runParser function so that it can be called
+// from the browser console if needed.
+window.runParser = runParser;
+// Run the parser as soon as the page's DOM has finished loading.
+window.onload = function () {
+  console.log("onload");
+  runParser();
+};
