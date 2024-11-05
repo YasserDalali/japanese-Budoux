67bc4dcc0912b0ba4d8cf69110d9ba45add850fa
YasserDalali <yasser.dalali.personal@gmail.com>
2024-11-05 10:15:06 +0100

    built app

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ export default defineConfig({
   base: '/japanese-Budoux/', // Change 'your-repo' to your actual GitHub repo name
   build: {
     outDir: 'dist', // This is the folder where build files will go
-  },
+  }
 })
